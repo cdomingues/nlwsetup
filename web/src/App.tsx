679aedf175712fcd8714a910d5eb157507712987
Carlos <domingues_Santos@hotmail.com>
2023-02-07 16:21:39 -0300

    continuando dev mobile

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,21 @@
-import { Habit } from "./components/Habit";
+import { HabitDay } from "./components/HabitDay";
 import './styles/global.css';
 
-function App() {
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+export function App() {
   
 
   return (
-    <div>
-    <Habit completed={3}/>
-    <Habit completed={10}/>
-    <Habit completed={30}/>
-    <Habit completed={30}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+      <Header />
+      <SummaryTable />
+      </div>
+        
     
     </div>
   )
 }
 
-export default App
+
